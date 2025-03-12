2fa3968edab04a1e2e266437f7d40b6773729a4d
m-d-webdev <mstph.iderkaoui@gmail.com>
2025-03-12 15:21:59 +0100

    amelioration

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response("welcome in kratritLivres back-office");
 });
+Route::get('/tset', function () {
+    return view("GES_LOACTION.index");
+});
+
